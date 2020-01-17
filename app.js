e499c04b7e6f39bce6fc60d30d3bff3e3f714a1f
Mika <salinkangas.mika@seamk.fi>
2020-01-17 17:19:13 +0200

    commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+let app = express();
+
+app.use((req, res, next) => {
+    console.log(`PATH: ${req.path}`);
+    next();
+});
+
+app.get('/', (reg, res, next) => {
+    res.send('Hello world');
+});
+
+app.use((req, res, next) => {
+    console.log('404');
+    res.status(404);
+    res.send('page not found')
+});
+
+app.listen(PORT);
